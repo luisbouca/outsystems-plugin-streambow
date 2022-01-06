c307cad76a74d31fa13032363c5eec4cecf1524d
André Grillo <andregrillo@gmail.com>
2022-01-06 17:10:31 +0000

    Android SDK new version fixed

--- src/android/Streambow.java
+++ src/android/Streambow.java
@@ -12,13 +12,13 @@ import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
-public class Streambow extends CordovaPlugin implements TestCallback {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Streambow extends CordovaPlugin {
     private static final String TAG = "StreambowPlugin";
-    //Context context = IS_AT ? cordova.getActivity().getWindow().getContext() : cordova.getActivity().getApplicationContext();
-//    Context context = this.cordova.getActivity().getApplicationContext();
-//    Context context = this.cordova.getContext().getApplicationContext();
-//    private Xperience xperience = Xperience.getInstance(context);
     private Xperience xperience;
     private CallbackContext callbackContext;
 
@@ -26,21 +26,41 @@ public class Streambow extends CordovaPlugin implements TestCallback {
 
         @Override
         public void progressUpdate(TestProgress testProgress, TestStatus testStatus) {
+            Log.i(TAG, ">>> Test Progress: " + testProgress.getProgress() + "% <<<");
+            Log.i(TAG, ">>> Test Message: " + testProgress.getMessage() + " <<<");
             switch (testStatus) {
+                case TEST_PROGRESS:
+                    if (testProgress.getProgress() == 100) {
+                        Log.i(TAG, ">>>Test FINISHED <<<");
+                        String results = "";
+                        if (testProgress.getResult() != null) {
+                            Log.i(TAG, ">>> RESULT STRING JSON: " + testProgress.getResult().getJSON() + " <<<");
+                            results = testProgress.getResult().toString();
+                            callbackContext.success(results);
+                        } else {
+                            Log.i(TAG, ">>> GET RESULT IS NULL <<<");
+                            callbackContext.error("Error: Test results are null");
+                        }
+                    }
+                    break;
+                case TEST_STARTED:
+                    Log.i(TAG, ">>> Test STARTED <<<");
+                    break;
                 case TEST_FINISHED:
-                    Log.i(TAG, "\n>>>Test done <<<\n");
-                    callbackContext.success("TEST DONE!");
+                    Log.i(TAG, ">>>Test FINISHED <<<");
+                    break;
                 case TEST_CANCELLED:
-                    Log.i(TAG, "\n>>>Test cancelled <<<\n");
-                    callbackContext.error("ERROR!");
+                    Log.i(TAG, ">>>Test cancelled <<<");
+                    callbackContext.error("Error: Test Cancelled");
+                    break;
             }
         }
     };
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    this.callbackContext = callbackContext;
-    if (action.equals("performTest")) {
+        this.callbackContext = callbackContext;
+        if (action.equals("performTest")) {
             String testID = args.getString(0);
             Log.i(TAG, "\n>>> testID: " + testID + " <<<\n");
             this.performTest(testID, callbackContext);
@@ -50,31 +70,12 @@ public class Streambow extends CordovaPlugin implements TestCallback {
     }
 
     private void performTest(String testID, CallbackContext callbackContext) {
-//        if (Build.VERSION.SDK_INT >= 23) {
-//            final int READ_PHONE_STATE = ActivityCompat.checkSelfPermission(cordova.getContext(), Manifest.permission.READ_PHONE_STATE);
-//
-//            if (READ_PHONE_STATE != PackageManager.PERMISSION_GRANTED) {
-//                Log.i(TAG, "\n>>> Permission not Granted <<<\n");
-//                ActivityCompat.requestPermissions(cordova.getActivity(), new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
-//
-//            } else {
-//                Log.i(TAG, "\n>>> Permission Granted <<<\n");
-//            }
-//        }
-
-        //this.xperience = Xperience.getInstance(this.cordova.getContext());
         this.xperience = Xperience.getInstance(this.cordova.getContext());
-        //this.xperience.startTest(testCallback, testID);
-        //if (this.xperience.startTest(this.testCallback, testID)){
-        if (this.xperience.startTest(this, testID)){
+        Xperience.preStart(this.cordova.getContext());
+        if (this.xperience.startTest(this.testCallback, testID)){
             Log.i(TAG, "\n>>> Service Requested <<<\n");
         } else {
             Log.i(TAG, "\n>>> Couldn't start service <<<\n");
         }
-    }
-
-    @Override
-    public void progressUpdate(TestProgress testProgress, TestStatus testStatus) {
-        Log.i(TAG, "\n>>> Progress Update <<<\n");
     }
 }
